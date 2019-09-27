53f0d19730ad11c86d1b97eff0c8d64ba86198bc
OfficialCRUGG <ich@crugg.de>
2019-09-27 15:38:30 +0200

    :sparkles: Extension Content Script

---
+++ extension/js/theme.js
@@ -0,0 +1,6 @@
+// document.head.append("<link rel='stylesheet' type='text/css' href='https://cdn.jsdelivr.net/gh/OfficialCRUGG/better-listen.moe@master/css/v1.css'>");
+let styleSheet = document.createElement("link");
+styleSheet.rel = "stylesheet";
+styleSheet.href = "https://cdn.jsdelivr.net/gh/OfficialCRUGG/better-listen.moe@master/css/v1.css";
+document.head.appendChild(styleSheet);
+console.log("Test");
